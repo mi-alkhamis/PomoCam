1d36484234233b4164136437d2a3d2c2e5e1127d
Milad Alkhamis <mi-alkhamis@hotmail.com>
2024-01-25 14:26:18 +0330

    create pomodoro cycle as simplest way

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import time
 from utility import s2ms
 
 # -------------------------------------- Conffig --------------------------------------
-# WORK_TIME = 25 * 60
-WORK_TIME = 5
+WORK_TIME = 25 * 60
 SHORT_BREAK = 5 * 60
 LONG_BREAK = 15 * 60
-MAX_CYCLE = 4
+MAX_CYCLE = 4 * 60
+WORK_MSG = "Work time"
+BREAK_MSG = "Take a Break"
 
 
 # ---------------------------------------- Main ----------------------------------------
@@ -17,61 +18,47 @@ class Pomodoro:
         self.short_break_time = SHORT_BREAK  # Hold Short break time
         self.long_break_time = LONG_BREAK  # Hold long Break timer
         self.max_cycle = MAX_CYCLE  # Maximum short break
-        self.type = "work"  # Hold Current type of session
-        self.msg = "Work time"
+        self.session_type = "work"  # Hold Current type of session
+        self.msg = WORK_TIME
         # init some var to use in pomodoro
 
-    def timer(self, timer, type, msg):
+    def timer(self, timer, session_type, msg):
         while timer:
             timer -= 1
             time.sleep(1)
             remaining_time = s2ms(timer)
             print(f"{msg}: {remaining_time[0]}:{remaining_time[1]}")
+        self.update(session_type)
 
     def start_pomo(self):
-        self.type = "work"
-        self.timer(self.work_time, self.type, self.msg)
+        self.session_type = "work"
+        self.msg = "Work time"
+        self.timer(self.work_time, self.session_type, self.msg)
         self.type = self.update(self.type)
 
-    def time_break(self,type):
-        self.type = type
-        if type == "rest":
-            self.timer(self.short_break_time,self.type,self.msg)
-        elif type == "long":
-            self.timer(self.long_break_time,self.type,self.msg)
+    def time_break(self, session_type):
+        self.session_type = session_type
+        if self.session_type == "short":
+            self.msg = BREAK_MSG
+            self.timer(self.short_break_time, self.session_type, self.msg)
+        elif self.session_type == "long":
+            self.msg = BREAK_MSG
+            self.timer(self.long_break_time, self.session_type, self.msg)
         self.type = self.update(self.type)
 
-    def update(self, type):
-        match type:
+    def update(self, session_type):
+        match session_type:
             case "work":
-                type = "rest"
-                if self.cycle == 4:
-                    type = "long"
-                    self.cycle = 0
+                self.cycle += 1
+                if self.cycle >= self.max_cycle:
+                    self.time_break("long")
                 else:
-                    self.cycle += 1
-
-            case "rest":
-                self.cylce += 1
+                    self.time_break("short")
+            case "short":
                 self.start_pomo()
-
             case "long":
                 self.cycle = 0
-                type = "work"
-        return type
-
-    def update(self, type):
-        if type == "work":
-            self.cycle += 1
-            if self.cycle >= self.max_cycle:
-                return "long"
-            else:
-                return "rest"
-        elif type == "rest":
-            return "work"
-        elif type == "long":
-            self.cycle = 0
-            return "work"
+                self.start_pomo()
 
 
 pomo = Pomodoro()
