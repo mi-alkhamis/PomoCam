8491273ad8c80bd859dbc8ec3a3b42e5d6d03546
Milad Alkhamis <mi-alkhamis@hotmail.com>
2024-01-26 23:36:23 +0330

    Add docstring to Pomodoro Class

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ BREAK_MSG = "Take a Break"
 # ---------------------------------------- Main ----------------------------------------
 class Pomodoro:
     def __init__(self):
+        """
+        A class to represent a Pomodoro timer.
+
+        Attributes
+        ----------
+        cycle : int
+            A counter of sessions before a long break.
+        work_time : int
+            Work time in seconds.
+        short_break_time : int
+            Short break time in seconds.
+        long_break_time : int
+            Long break time in seconds.
+        max_cycle : int
+            Maximum number of cycles before a long break.
+        session_type : str
+            Current type of session, can be "work", "short", or "long".
+        msg : str
+            Message to display during the session.
+        """
+
         self.cycle = 0  # counter of seseions before Long Break
         self.work_time = WORK_TIME  # Hold Work time by secinds
         self.short_break_time = SHORT_BREAK  # Hold Short break time
@@ -23,6 +44,19 @@ class Pomodoro:
         # init some var to use in pomodoro
 
     def timer(self, timer, session_type, msg):
+        """
+        Counts down the timer for the given session type and displays a message.
+
+        Parameters
+        ----------
+        timer : int
+            The countdown time in seconds.
+        session_type : str
+            The type of the session, can be "work", "short", or "long".
+        msg : str
+            The message to display during the countdown.
+        """
+
         while timer:
             timer -= 1
             time.sleep(1)
@@ -31,12 +65,23 @@ class Pomodoro:
         self.update(session_type)
 
     def start_pomo(self):
+        """
+        Starts a work session of the Pomodoro timer.
+        """
         self.session_type = "work"
         self.msg = "Work time"
         self.timer(self.work_time, self.session_type, self.msg)
         self.type = self.update(self.type)
 
     def time_break(self, session_type):
+        """
+        Starts a break session of the Pomodoro timer.
+
+        Parameters
+        ----------
+        session_type : str
+            The type of the break, can be "short" or "long".
+        """
         self.session_type = session_type
         if self.session_type == "short":
             self.msg = BREAK_MSG
@@ -47,6 +92,19 @@ class Pomodoro:
         self.type = self.update(self.type)
 
     def update(self, session_type):
+        """
+        Updates the session type and cycle count based on the current session.
+
+        Parameters
+        ----------
+        session_type : str
+            The current session type, can be "work", "short", or "long".
+
+        Returns
+        -------
+        str
+            The updated session type.
+        """
         match session_type:
             case "work":
                 self.cycle += 1
